b44d1398bc8afb55179ff88b03e016289c69d18e
Aruna <aruna.evan15@gmail.com>
2021-09-22 14:43:17 -0700

    commit tree 1d080d739af2b1a41d840f5fe3c821630f8e52ed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(allDrivers, findDriver) {
+    return allDrivers.filter(
+        (driver)=> driver.toUpperCase() === findDriver.toUpperCase()
+    );
+}
+
+function fuzzyMatch(allDrivers, letters) {
+    return allDrivers.filter(
+        (driver) => (driver.slice(0,letters.length)) === letters
+    );
+}
+
+function matchName(allDrivers, findName) {
+    return allDrivers.filter(
+        (driver) => driver.name === findName
+    );
+}
